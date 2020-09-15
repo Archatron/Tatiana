cd82fba8f8749f68837adcd86b5f56edf60a428d
Archatron <68031930+Archatron@users.noreply.github.com>
2020-09-15 09:43:42 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -2,11 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client
 module.exports = {
     name: 'purge',
-    execute(message,  args){
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.send("You can't use this command!");
-    if(!args[0]) return message.channel.send("Specify how many messages you want to delete.");
-    message.delete();
-    message.channel.bulkDelete(args[0]).catch(e => { message.channel.send("You can only delete 100 messages at once.")});
-    message.channel.send(`Succesfuly deleted \` ${args[0]} messages\``).then(m => message.delete({ timeout: 5000}));
+    execute(message, args){
+      if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You can't use this command!");
+      if(!args[1]) return message.channel.send("Specify how many messages you want to delete.");
+      message.channel.bulkDelete(args[1]);
+      message.channel.send(`Successfully deleted \`${args[1]} messages\``).then(m => message.delete({ timeout: 5000}));
     }
 }
